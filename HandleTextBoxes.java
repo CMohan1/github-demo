a1a97a5c142a5d773a4b48a9ca7d6fd580c38ef9
mohan <mohanchundi567@gmail.com>
2022-11-26 19:29:57 +0530

    commit

--- HandleTextBoxes.java
+++ HandleTextBoxes.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
- * @author Yadagiri Reddy
+ 
  * How to handle the textboxes using selenium webdriver?
  */
 public class HandleTextBoxes {
